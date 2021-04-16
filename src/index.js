be9140fecc44acabe3d64cfacc5de3629856c29b
Serhii <serhii.nikiforenko@gmail.com>
2021-04-17 00:18:00 +0300

    testing JSX

--- src/index.js
+++ src/index.js
@@ -2,28 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const TodoList = () => {
+
+  const items = ['Learn React', 'Build Awesome App'];
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
     </ul>
   );
 };
 
 const AppHeader = () => {
-  return <h1>My Todo List</h1>;			// если одна строка, то () ставить не нужно
+  return <h1>My Todo List</h1>;
 };
 
 const SearchPanel = () => {
-  return <input placeholder="search"/>;
+
+  const searchText = 'Type here to search';
+  const searchStyle = {
+    fontSize: '25px'
+  }
+  return <input
+    style = { searchStyle }		      // нужно передавать обьект
+    placeholder={ searchText } />
 };
 
 const App = () => {
+
+  const isLoggedIn = true;
+  const loginBox = <span>Log in please</span>;
+  const welcomeBox = <span>Welcome Back</span>;
+
   return (
     <div>
+      { isLoggedIn ? welcomeBox : loginBox }		{/* true - ел. нет, false - ел. есть 
+                                                  Должны быть простые выражения*/}
       <AppHeader />
       <SearchPanel />
-      <TodoList />      {/*теперь имя этого компонента можно исп. как отдельный html-тег */}
+      <TodoList items={['Item 1', 'Item 2']}/>
     </div>
   );
 };
