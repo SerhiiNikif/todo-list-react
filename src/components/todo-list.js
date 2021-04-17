92d01c1add0368e48e9b5556d76f5c80967386fa
Serhii <serhii.nikiforenko@gmail.com>
2021-04-17 12:22:12 +0300

    using bootstrap

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,29 +1,25 @@
 import React from 'react';
+
 import TodoListItem from './todo-list-item';
-/*
-Warning был потому, что я вынес елементы списка в отдельный массив - 
-это массив реакт-елементов, а потом использовал этот массив в JSX разметке.
-Реакт хочет, чтобы у каждого елемента li был уникальный ключ, свойство key
-*/
+import './todo-list.css';
+
 const TodoList = ({ todos }) => {
 
-    const elements = todos.map((item) => {
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
-    const { id, ...itemProps } = item;	/* сюда ввойдут все те свойства, которые не были 
-                                        деструктурированы в этом выражении. 
-                                        А в моем случаи это все свойства кроме id */
-      return (
-        <li key={ id }>	{/* в реальном проекте key буду получать как id, напр. из БД */}
-          <TodoListItem { ...itemProps } />     {/* теперь id не будет передаваться в TodoListItem */}
-        </li>
-      );
-    });
-  
     return (
-        <ul>
-            { elements }
-        </ul>
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
     );
-  };
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
+};
 
 export default TodoList;
