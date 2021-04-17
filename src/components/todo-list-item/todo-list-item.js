33a618f4b008a2636cd17793d96d8c9ff7b69395
Serhii <serhii.nikiforenko@gmail.com>
2021-04-17 15:12:08 +0300

    changing the class using state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,39 +1,54 @@
 import React, { Component } from 'react';
 import './todo-list-item.css';
 
-export default class TodoListItem extends Component {
+export default class TodoListItem extends Component { 
 
-    onLabelClick = () => {				// создаю ф. не на прототипе класса, а на самом обьекте
-      console.log(`Done: ${this.props.label}`);
+    state = {
+      done: false   // После того как я первый раз проинициализировал state,
+    };              // его болше нельзя изменять напрямую
+
+    onLabelClick = () => {
+        this.setState({		/* сюда можно передать объект с изменениями, которые хотим 
+                            внести в state. Что мы хотим поменять? Когда польз. кликает
+                            на нашем label, мы сменим состояния */
+          done: true
+        });
     };
   
     render () {	
-      const { label, important = false } = this.props;
-  
-      const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-      };
-  
-      return (
-        <span className="todo-list-item">
-          <span
-            className="todo-list-item-label"
-            style={style}
-            onClick={ this.onLabelClick }>
-            {label}
-        </span>
+        const { label, important = false } = this.props;
+        const { done } = this.state;			// деструктурируем
+                                                // state можно читать, но нельзя изменять напрямую
 
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-          <i className="fa fa-exclamation" />
-        </button>
+        let classNames = 'todo-list-item';	    // вынесем назв. класса в перепенную
+        if(done) {
+            classNames += ' done';		        // с условием конкатинируем классы
+        }
 
-        <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o" />
-        </button>
-      </span>
-    );
-  };
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+
+        return (
+            <span className={ classNames }>		{/* сразу же используем */}
+            <span
+                className="todo-list-item-label"
+                style={style}
+                onClick={ this.onLabelClick }>
+                {label}
+            </span>
+
+            <button type="button"
+                    className="btn btn-outline-success btn-sm float-right">
+                <i className="fa fa-exclamation" />
+            </button>
+
+            <button type="button"
+                    className="btn btn-outline-danger btn-sm float-right">
+                <i className="fa fa-trash-o" />
+            </button>
+            </span>
+        );
+    };
 }
