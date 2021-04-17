16d91ea7a3fccc052e2823c5cd5e0badc598bf18
Serhii <serhii.nikiforenko@gmail.com>
2021-04-17 11:12:12 +0300

    react project structure

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-
-  const items = ['Learn React', 'Build Awesome App'];
-  return (
-    <ul>
-      <li>{ items[0] }</li>
-      <li>{ items[1] }</li>
-    </ul>
-  );
-};
-
-const AppHeader = () => {
-  return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-
-  const searchText = 'Type here to search';
-  const searchStyle = {
-    fontSize: '25px'
-  }
-  return <input
-    style = { searchStyle }		      // нужно передавать обьект
-    placeholder={ searchText } />
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
-
-  const isLoggedIn = true;
-  const loginBox = <span>Log in please</span>;
-  const welcomeBox = <span>Welcome Back</span>;
-
-  return (
-    <div>
-      { isLoggedIn ? welcomeBox : loginBox }		{/* true - ел. нет, false - ел. есть 
-                                                  Должны быть простые выражения*/}
-      <AppHeader />
-      <SearchPanel />
-      <TodoList items={['Item 1', 'Item 2']}/>
-    </div>
-  );
-};
-
+  
+    return (
+      <div>
+        <AppHeader />
+        <SearchPanel />
+        <TodoList />
+      </div>
+    );
+  };
+  
 ReactDOM.render(<App />, document.getElementById('root'));
