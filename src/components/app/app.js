ea092bac931fc0e82c20b3cf82acbc00d0203af6
Serhii <serhii.nikiforenko@gmail.com>
2021-04-17 16:26:24 +0300

    setState() - deleting an element

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,33 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
-
 import './app.css';
 
-const App = () => {
+/* Для того чтобы реакт обновил приложения App нужно сделать комп. классом, 
+а todoData сделать частью state */
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+export default class App extends Component {
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
+    state = {
+        todoData: [
+          { label: 'Drink Coffee', important: false, id: 1 },
+          { label: 'Make Awesome App', important: true, id: 2 },
+          { label: 'Have a lunch', important: false, id: 3 }
+        ]
+    };
 
-      <TodoList 
-        todos={todoData} 
-        onDeleted={(id) => console.log('del', id)}/>    {/* будет получать id ел. который мы удаляем */}
+    deleteItem = (id) => {		// передаем сюда ф. потому что нам нужно установить сюда новый state
+                                // А новый state это старий массив todoData без одного елемента
+                                // наш новый state зависит от старого state, потому что нам нужно знать какой
+                                // был старый массив
+        this.setState(({ todoData }) => {
+            // Для того чтобы вычислить новый state, нам нужно удалить из массива елемент
+	        // Для начала получим индекс того елемента, которого мы собрались удалять
+            const idx = todoData.findIndex((el) => el.id === id);   // будем искать индекс ел. у которого id такой же 
+                                                                    // как тот id который мы получили
+            // Теперь у нас есть индекс ел. который мы хотим удалять
+            console.log(id);
+            // Когда будем кликать на корзину в консоле будут индексы елементов
+
+            const before = todoData.slice(0, idx)		// будем использовать метод slice, он не изменяет существующий массив
+            const after = todoData.slice(idx + 1);	    // idx + 1 и до конца
+            const newArray = [...before, ...after];
+
+            return {	// Ну и нужно вернуть новое состояния
+                todoData: newArray
+            }		
+        });
+        // Элементы удаляються
+    };
+
+    render() {
+    return (
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex">
+            <SearchPanel />
+            <ItemStatusFilter />
+        </div>
+
+        <TodoList 
+            todos={this.state.todoData}         // теперь todoData - это часть state
+            onDeleted={ this.deleteItem }/>     {/* Когда будем кликать на корзину в консоле будут id елементов */}
     </div>
-  );
+    );
 };
-
-export default App;
+}
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react';
 import './todo-list-item.css';
 
-// Сделаем так, чтобы могли удалаять елементы со списка
-
-/* Для того, чтобы удалить ел. внизу иерархии на нужно сделать так, чтобы данные вверху иерархии
-на уровне App обновились
-
-Нам нужно сделать так, чтобы компонент App узнавал, что кнопка на компоненте TodoListItem была
-нажата
-
-Сделаем так, что TodoListItem будет генирить события, которые будет слушать TodoList. TodoList
-передаст это событие еще выше в ел. App и ел. App обновит массив, и затем как массив обновиться
-TodoListItem исчезнет */
-
 export default class TodoListItem extends Component { 
 
     state = {
@@ -64,8 +52,7 @@ export default class TodoListItem extends Component {
 
             <button type="button"
                 className="btn btn-outline-danger btn-sm float-right"
-	            onClick={ onDeleted }>		{/* вызовем ф. которую нам передали в props из ел. выше 
-                                            Теперь при нажатии на корзину выводиться в консоль Deleted */}
+	            onClick={ onDeleted }>
           <i className="fa fa-trash-o" />
         </button>
             </span>
