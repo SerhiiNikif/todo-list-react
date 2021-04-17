68ae3699b9c7e14128ebe4a178416112adceb350
Serhii <serhii.nikiforenko@gmail.com>
2021-04-17 17:40:07 +0300

    setState() - adding items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,12 +4,12 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 import './app.css';
 
-/* Для того чтобы реакт обновил приложения App нужно сделать комп. классом, 
-а todoData сделать частью state */
-
 export default class App extends Component {
+    // id нам нужно згенерить самостоятельно
+    maxId = 100;
 
     state = {
         todoData: [
@@ -19,43 +19,59 @@ export default class App extends Component {
         ]
     };
 
-    deleteItem = (id) => {		// передаем сюда ф. потому что нам нужно установить сюда новый state
-                                // А новый state это старий массив todoData без одного елемента
-                                // наш новый state зависит от старого state, потому что нам нужно знать какой
-                                // был старый массив
+    deleteItem = (id) => {
         this.setState(({ todoData }) => {
-            // Для того чтобы вычислить новый state, нам нужно удалить из массива елемент
-	        // Для начала получим индекс того елемента, которого мы собрались удалять
-            const idx = todoData.findIndex((el) => el.id === id);   // будем искать индекс ел. у которого id такой же 
-                                                                    // как тот id который мы получили
-            // Теперь у нас есть индекс ел. который мы хотим удалять
-            console.log(id);
-            // Когда будем кликать на корзину в консоле будут индексы елементов
-
-            const before = todoData.slice(0, idx)		// будем использовать метод slice, он не изменяет существующий массив
-            const after = todoData.slice(idx + 1);	    // idx + 1 и до конца
+           
+            const idx = todoData.findIndex((el) => el.id === id); 
+            const before = todoData.slice(0, idx);
+            const after = todoData.slice(idx + 1);
             const newArray = [...before, ...after];
 
-            return {	// Ну и нужно вернуть новое состояния
+            return {
                 todoData: newArray
             }		
         });
-        // Элементы удаляються
+    };
+
+    addItem = (text) => {
+        // generate id?
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++		// может показаться, что мы меняем состояния класса
+                                    // на самом деле maxId - это не состояние класса, он не находиться
+                                    // в обьекте state
+        };
+
+        // Мы не можем изменять массив и изменять предыдущее состояние
+        this.setState(({todoData}) => {
+            // Новый масиив будет состоять из всех ел. старого массива + новый елемент
+            const newArr = [
+                ...todoData,
+                newItem
+            ];
+
+            return {
+                todoData: newArr
+            };
+        });
     };
 
     render() {
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-            <SearchPanel />
-            <ItemStatusFilter />
-        </div>
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
 
-        <TodoList 
-            todos={this.state.todoData}         // теперь todoData - это часть state
-            onDeleted={ this.deleteItem }/>     {/* Когда будем кликать на корзину в консоле будут id елементов */}
-    </div>
-    );
-};
+                <TodoList 
+                    todos={this.state.todoData}
+                    onDeleted={ this.deleteItem }/>
+
+                <ItemAddForm onItemAdded={this.addItem}/>
+            </div>
+        );
+    }
 }
